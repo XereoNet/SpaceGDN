374aebbffed68d504bc55a1de0bbfc74b8ca2b68
Connor Peet <connor@connorpeet.com>
2013-12-02 18:58:29 -0500

    Last build retrieve and bugfixes

--- loader/loader.py
+++ loader/loader.py
@@ -1,4 +1,6 @@
-import os
+import os, importlib
+
+from gdn.db import connection, models
 
 _path = os.path.dirname(os.path.realpath(__file__))
 
@@ -7,20 +9,30 @@ def loadSources():
 	import glob, json
 
 	files = glob.glob(_path + '/../sources/*.json')
-	output = {}
+	output = []
 
 	for f in files:
 
 		with open(f) as handle:
-			contents = json.load(handle)
-
-		output[ contents['name'] ] = contents
+			output.append(json.load(handle))
 
 	return output
+
+def getLastBuild(data):
+	return connection.session.query(models.Channel)\
+		.filter(models.Channel.name == data['name'])\
+		.join(models.Version, models.Version.channel_id == models.Channel.id)\
+		.join(models.Build, models.Build.version_id == models.Version.id)\
+		.order_by(models.Build.build.desc())\
+		.first()
 
 def load():
 	sources = loadSources()
 
-	for source in sources.iteritems():
-		#print 'Loading source ' + source['name']
-		print 'Loading'
+	for source in sources:
+		print 'Loading source "%s"' % source['name']
+
+		for channel in source['channels']:
+			module = importlib.import_module('loader', channel['interface'])
+
+			last = getLastBuild(channel)
