b8e8319473180272770256d0329450a11e3ba967
Connor Peet <connor@connorpeet.com>
2013-12-13 10:00:00 -0500

    Fixed depends, import Tornado WSGI server

--- app/gdn/manage.py
+++ app/gdn/manage.py
@@ -10,6 +10,15 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
+	from tornado.wsgi import WSGIContainer
+	from tornado.httpserver import HTTPServer
+	from tornado.ioloop import IOLoop
+
+	http_server = HTTPServer(WSGIContainer(app))
+	http_server.listen(app.config['HTTP_PORT'])
+	IOLoop.instance().start()
+
+def debug():
 	app.run(debug = app.config['DEBUG'], host=app.config['HTTP_HOST'], port=app.config['HTTP_PORT'])
 
 @manager.command
