5c36fef32df8f755d0958c6eef37cbc9887513fe
Connor Peet <connor@connorpeet.com>
2014-08-22 21:14:04 -0500

    Bump version!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.sysconfig import get_python_lib
 
 setup(
     name='SpaceGDN',
-    version='2.0.0-dev',
+    version='2.0.0-rc1',
     url='https://github.com/connor4312/querytree',
     author='Connor Peet',
     author_email='connor@connorpeet.com',
