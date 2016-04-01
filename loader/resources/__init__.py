b172f9ec3c8e67667a03a8ef3fa87e5fe8199b6a
totokaka <mail@totokaka.io>
2016-04-01 23:11:04 +0200

    Remove Cauldron and CraftBukkit loaders, as they are dead.

--- loader/resources/__init__.py
+++ loader/resources/__init__.py
@@ -1,7 +1,5 @@
 from .creeperrepo_loader import CreeperRepo
 from .mojang_loader import Mojang
-from .cauldron_loader import Cauldron
-from .craftbukkit_loader import CraftBukkit
 from .technic_loader import Technic
 from .direct_loader import Direct
 from .jenkins_maven_loader import JenkinsMaven
@@ -9,8 +7,6 @@ from .jenkins_maven_loader import JenkinsMaven
 loaders = {
     'creeperrepo': CreeperRepo(),
     'mojang': Mojang(),
-    'cauldron': Cauldron(),
-    'craftbukkit': CraftBukkit(),
     'technic': Technic(),
     'direct': Direct(),
     'jenkins_maven': JenkinsMaven()
