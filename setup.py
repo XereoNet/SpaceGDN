69ca03671db09171c553e1b48e5b745c10fd5be8
totokaka <mail@totokaka.io>
2016-04-01 20:49:11 +0200

    Define version numbers in setup.py.
    Let's prevent messy deployings in the future.

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     license='AGPL-3.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['Flask',
-                      'Flask-Script',
-                      'uwsgi',
-                      'requests',
-                      'beautifulsoup4',
-                      'lxml',
-                      'raven[flask]',
-                      'pymongo']
+    install_requires=['Flask==0.10.1',
+                      'Flask-Script==2.0.5',
+                      'uwsgi==2.0.12',
+                      'requests==2.9.1',
+                      'beautifulsoup4==4.4.1',
+                      'lxml==3.6.0',
+                      'raven[flask]==5.12.0',
+                      'pymongo==3.2.2']
 )
